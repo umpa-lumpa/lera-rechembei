9c946fa371b990f63b596e2a487bea24d130c19e
LERA <nlo_lirik@mail.ru>
2016-02-06 19:41:11 +0300

    3

--- binary-tree.js
+++ binary-tree.js
@@ -43,10 +43,61 @@ contains(data) {
 	}		
 }	
 remove(data) {
-
+if(this.contains(data)){
+	var ps=this.root, pr=this.root,w,v;
+	while(ps.data!=data){
+		pr=ps;
+		if(data>ps.data) ps=ps.right;
+		else ps=ps.left;
 	}
-
-	
+	if((ps.left===null) && (ps.right===null)){
+		if(pr===ps)
+			this.root=null;
+		else if(pr.right===ps) 
+			pr.right=null;
+		else 
+			pr.left=null;
+	}
+	else if (ps.right===null){
+		if(pr===ps){
+			ps=ps.left;
+			this.root=ps;}
+		else if(pr.right===ps) 
+			pr.right=ps.left;
+		else 
+			pr.left=ps.left;
+	}
+	else if(ps.left===null){
+		if (pr===ps){
+			ps=ps.right;
+			this.root=ps;}
+		else if (pr.right===ps)
+			pr.right=ps.right;
+		else
+			pr.left=ps.right;
+	}
+	else{
+		w=ps.left;
+		if(w.right===null)
+			w.right=ps.right;
+		else{
+			while(w.right!=null){
+				v=w;
+				w=w.right;
+			}
+			v.right=w.left;
+			w.right=ps.right;
+			w.left=ps.left;
+		}
+		if(ps===pr)
+			this.root=w;
+		else if (pr.left===ps)
+			pr.left=w;
+		else 
+			pr.right=w;
+	}
+}
+}	
 size() {
 
 	}
