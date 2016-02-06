402ec9ff250d52db2a16cc1c87221399e178c50c
LERA <nlo_lirik@mail.ru>
2016-02-06 18:00:14 +0300

    1

--- binary-tree.js
+++ binary-tree.js
@@ -1,27 +1,46 @@
 'use strict';
 
+
+
 class BinaryTree {
-	constructor() {
+	
+constructor() {
+	this.root=null;
+}
+insert(data) {
+	var newel=new Node(data,null,null);
+		if(this.root===null){
+			this.root=newel;
+		}
+		else{
+			var r=this.root;
+			while(r!=null){
+				var ro=r;
+				var b=(r.data>data);
+				if (b) r=r.left;
+				else r=r.right;
+			}	
+			if(b) ro.left=newel;
+			else  ro.right=newel;
+		}
+}
+contains(data) {
 
 	}
 
-	insert(data) {
+	
+remove(data) {
 
 	}
 
-	contains(data) {
+	
+size() {
 
 	}
 
-	remove(data) {
+	
+isEmpty() {
 
 	}
 
-	size() {
-
-	}
-
-	isEmpty() {
-
-	}
 }
